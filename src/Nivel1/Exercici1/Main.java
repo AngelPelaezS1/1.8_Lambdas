b0b23af1b942ac3417724d3813e1e3d020764eae
AngelPelaezSanz <angelpelaezs11@gmail.com>
2024-12-08 18:31:01 +0100

    acabado ejercicio 1

---
+++ src/Nivel1/Exercici1/Main.java
@@ -0,0 +1,18 @@
+package Nivel1.Exercici1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> countries = Arrays.asList("Alemania", "Montenegro", "Croacia", "Noruega", "Canada", "Salvador");
+
+
+        List<String> filteredCountries = countries.stream()
+                .filter(s -> s.contains("o"))
+                .collect(Collectors.toList());
+
+        System.out.println(filteredCountries);
+    }
+}
